f6df3b8a0c5eb1ee1f61e741d6e1ce762f268e3a
xuetf <476122294@qq.com>
2018-05-08 19:32:34 +0800

    add config

---
+++ Config.py
@@ -0,0 +1,36 @@
+from Board import *
+from  Game import *
+from PolicyValueNet import *
+import pickle
+from collections import deque
+class Config:
+    def __init__(self):
+        self.board_width = 6
+        self.board_height = 6
+        self.n_in_row = 4
+        self.board = Board(width=self.board_width, height=self.board_height, n_in_row=self.n_in_row)
+        self.game = Game(self.board)
+        # training params
+        self.learn_rate = 2e-3
+        self.lr_multiplier = 1.0  # adaptively adjust the learning rate based on KL
+        self.lr_decay_per_iterations = 100  # learning rate decay after how many iterations
+        self.lr_decay_speed = 5  # learning rate decay speed
+        self.temp = 1.0  # the temperature param
+        self.n_playout = 400  # num of simulations for each move
+        self.c_puct = 5
+        self.buffer_size = 10000
+        self.batch_size = 512  # mini-batch size for training
+        self.data_buffer = deque(maxlen=self.buffer_size)
+        self.play_batch_size = 3  # how many games of each self-play epoch
+        self.per_game_opt_times = 5  # num of train_steps for each update
+        self.is_adjust_lr = True  # whether dynamic changing lr
+        self.kl_targ = 0.02  # KL，用于early stop
+        self.check_freq = 50  # frequency of checking the performance of current model and saving model
+        self.start_game_num = 0  # the starting num of training
+        self.game_batch_num = 1500
+        self.best_win_ratio = 0.0
+        # num of simulations used for the pure mcts, which is used as the opponent to evaluate the trained policy
+        self.pure_mcts_playout_num = 1000
+        self.network = ResNet  # the type of network
+        self.policy_param = None  # Network parameters
+        self.loss_records = [] # 记录损失
--- Train.py
+++ Train.py
@@ -14,52 +14,22 @@ from Board import Board
 from PolicyValueNet import *  # Pytorch
 from AlphaZeroPlayer import AlphaZeroPlayer
 from RolloutPlayer import RolloutPlayer
-
+from Config import *
 root_data_file = "data/"
 
 class TrainPipeline():
-    def __init__(self, init_model=None):
+    def __init__(self, config=None):
         # params of the board and the game
-        self.board_width = 6
-        self.board_height = 6
-        self.n_in_row = 4
-        self.board = Board(width=self.board_width, height=self.board_height, n_in_row=self.n_in_row)
-        self.game = Game(self.board)
-        # training params
-        self.learn_rate = 1e-3
-        self.lr_multiplier = 1.0  # adaptively adjust the learning rate based on KL
-        self.lr_decay_per_iterations = 100 # learning rate decay after how many iterations
-        self.lr_decay_speed = 5 # learning rate decay speed
-        self.temp = 1.0  # the temperature param
-        self.n_playout = 400  # num of simulations for each move
-        self.c_puct = 5
-        self.buffer_size = 5000
-        self.batch_size = 512  # mini-batch size for training
-        self.data_buffer = deque(maxlen=self.buffer_size)
-        self.play_batch_size = 3  # how many games of each self-play epoch
-        self.epochs = 5  # num of train_steps for each update
-        self.is_adjust_lr = True # whether dynamic changing lr
-        self.kl_targ = 0.025  # KL，用于early stop
-        self.check_freq = 50
-        self.game_batch_num = 1500
-        self.best_win_ratio = 0.0
-        # num of simulations used for the pure mcts, which is used as the opponent to evaluate the trained policy
-        self.pure_mcts_playout_num = 1000
-        self.network = ResNet  #the type of network
-
-        policy_param = None
-        if init_model:
-            print ('init model')
-            # start training from an initial policy-value net
-            policy_param = pickle.load(open(init_model, 'rb')) # resume
+        self.config = config if config else Config()
 
         # Network wrapper
-        self.policy_value_net = PolicyValueNet(self.board_width, self.board_height, net_params=policy_param,
-                                               Network=self.network)
-        # 传入policy_value_net的predict方法，神经网络辅助MCTS搜索过程
-        self.mcts_player = AlphaZeroPlayer(self.policy_value_net.predict, c_puct=self.c_puct,
-                                           nplays=self.n_playout, is_selfplay=True)
+        self.policy_value_net = PolicyValueNet(self.config.board_width, self.config.board_height,
+                                               net_params=self.config.policy_param,
+                                               Network=self.config.network)
 
+        # 传入policy_value_net的predict方法，神经网络辅助MCTS搜索过程
+        self.mcts_player = AlphaZeroPlayer(self.policy_value_net.predict, c_puct=self.config.c_puct,
+                                           nplays=self.config.n_playout, is_selfplay=True)
 
 
     def self_play(self, n_games=1):
@@ -70,27 +40,27 @@ class TrainPipeline():
         self.episode_len = 0
         self.augmented_len = 0
         for i in range(n_games):
-            winner, play_data, episode_len = self.game.start_self_play_game(self.mcts_player, temp=self.temp)
+            winner, play_data, episode_len = self.config.game.start_self_play_game(self.mcts_player, temp=self.config.temp)
             self.episode_len += episode_len # episode_len每局下的回合数
             # augment the data
             play_data = self.augment_data(play_data)
             self.augmented_len += len(play_data)
-            self.data_buffer.extend(play_data)
+            self.config.data_buffer.extend(play_data)
 
     def optimize(self, iteration=0):
         """update the policy-value net"""
-        mini_batch = random.sample(self.data_buffer, self.batch_size)
+        mini_batch = random.sample(self.config.data_buffer, self.config.batch_size)
         state_batch = [data[0] for data in mini_batch]
         mcts_probs_batch = [data[1] for data in mini_batch]
         winner_batch = [data[2] for data in mini_batch]
 
-        if self.is_adjust_lr:
+        if self.config.is_adjust_lr:
             old_probs, old_v = self.policy_value_net.predict_many(state_batch) # used for adjusting lr
 
-        for i in range(self.epochs):
+        for i in range(self.config.per_game_opt_times): # number of opt times
             loss_info = self.policy_value_net.fit(state_batch, mcts_probs_batch, winner_batch,
-                                                      self.learn_rate * self.lr_multiplier)
-        if self.is_adjust_lr:
+                                                      self.config.learn_rate * self.config.lr_multiplier)
+        if self.config.is_adjust_lr:
             # adaptively adjust the learning rate
             self.adjust_learning_rate(old_probs, old_v, state_batch, winner_batch)
             #self.adjust_learning_rate_2(iteration)
@@ -108,22 +78,22 @@ class TrainPipeline():
         '''
         new_probs, new_v = self.policy_value_net.predict_many(state_batch)
         kl = np.mean(np.sum(old_probs * (np.log(old_probs + 1e-10) - np.log(new_probs + 1e-10)), axis=1))  # KL散度，相对熵
-        if kl > self.kl_targ * 2 and self.lr_multiplier > 0.1:  # kl增大，收敛不好，减小学习率
-            self.lr_multiplier /= 1.5
-        elif kl < self.kl_targ / 2 and self.lr_multiplier < 10:  # kl很小，说明收敛不错；提高学习率
-            self.lr_multiplier *= 1.5
+        if kl > self.config.kl_targ * 2 and self.config.lr_multiplier > 0.1:  # kl增大，收敛不好，减小学习率
+            self.config.lr_multiplier /= 1.5
+        elif kl < self.config.kl_targ / 2 and self.config.lr_multiplier < 10:  # kl很小，说明收敛不错；提高学习率
+            self.config.lr_multiplier *= 1.5
         explained_var_old = 1 - np.var(np.array(winner_batch) - old_v.flatten()) / np.var(np.array(winner_batch))
         explained_var_new = 1 - np.var(np.array(winner_batch) - new_v.flatten()) / np.var(np.array(winner_batch))
 
         print("kl:{:.5f},lr:{},explained_var_old:{:.3f},explained_var_new:{:.3f}".format(
-                kl, self.learn_rate * self.lr_multiplier, explained_var_old, explained_var_new))
+                kl, self.config.learn_rate * self.config.lr_multiplier, explained_var_old, explained_var_new))
 
 
     def adjust_learning_rate_2(self, iteration):
         '''衰减法'''
-        if (iteration+1) % self.lr_decay_per_iterations == 0:
-            self.lr_multiplier /= self.lr_decay_speed
-        print ("lr:{}".format(self.learn_rate * self.lr_multiplier))
+        if (iteration+1) % self.config.lr_decay_per_iterations == 0:
+            self.config.lr_multiplier /= self.config.lr_decay_speed
+        print ("lr:{}".format(self.config.learn_rate * self.config.lr_multiplier))
 
 
     def evaluate(self, n_games=10):
@@ -131,16 +101,16 @@ class TrainPipeline():
         Evaluate the trained policy by playing games against the pure MCTS player
         Note: this is only for monitoring the progress of training
         """
-        current_mcts_player = AlphaZeroPlayer(self.policy_value_net.predict, c_puct=self.c_puct,
-                                              nplays=self.n_playout)
-        pure_mcts_player = RolloutPlayer(c_puct=5, nplays=self.pure_mcts_playout_num)  # 评估的时候应该跟当前最强的战斗
+        current_mcts_player = AlphaZeroPlayer(self.policy_value_net.predict, c_puct=self.config.c_puct,
+                                              nplays=self.config.n_playout)
+        pure_mcts_player = RolloutPlayer(c_puct=5, nplays=self.config.pure_mcts_playout_num)  # 可以优化，评估的时候应该跟当前最强的战斗
         win_cnt = defaultdict(int)
         for i in range(n_games):
             print ("evaluate game %d" %i)
-            winner = self.game.start_game(current_mcts_player, pure_mcts_player, who_first=i % 2, is_shown=0)
+            winner = self.config.game.start_game(current_mcts_player, pure_mcts_player, who_first=i % 2, is_shown=0)
             win_cnt[winner] += 1
         win_ratio = 1.0 * (win_cnt[1] + 0.5 * win_cnt[-1]) / n_games
-        print("num_playouts:{}, win: {}, lose: {}, tie:{}".format(self.pure_mcts_playout_num, win_cnt[1], win_cnt[2],
+        print("num_playouts:{}, win: {}, lose: {}, tie:{}".format(self.config.pure_mcts_playout_num, win_cnt[1], win_cnt[2],
                                                                   win_cnt[-1]))
         return win_ratio
 
@@ -167,7 +137,7 @@ class TrainPipeline():
             for i in [1, 2, 3, 4]:
                 # rotate counterclockwise
                 equi_state = np.array([np.rot90(s, i) for s in state])  # i=4就是原来的数据
-                equi_mcts_prob = np.rot90(np.flipud(mcts_porb.reshape(self.board_height, self.board_width)),
+                equi_mcts_prob = np.rot90(np.flipud(mcts_porb.reshape(self.config.board_height, self.config.board_width)),
                                           i)  # 上下翻转成棋盘形状，各个cell的值对应该位置下棋概率
                 extend_data.append((equi_state, np.flipud(equi_mcts_prob).flatten(), winner))
                 # flip horizontally
@@ -178,45 +148,47 @@ class TrainPipeline():
 
     def save_model(self, win_ratio, epochs, prefix=''):
         # save
-        net_params = self.policy_value_net.get_policy_param()  # get model params
-        pickle.dump(net_params, open(root_data_file + prefix + "current_policy_{}_epochs_{}.model".format(self.policy_value_net, epochs), 'wb'),
-                    pickle.HIGHEST_PROTOCOL)  # save model param to file
+        self.config.policy_param = self.policy_value_net.get_policy_param()  # get model params
 
-        if win_ratio > self.best_win_ratio:
+        if win_ratio > self.config.best_win_ratio:
             print("New best policy!!!!!!!!")
-            self.best_win_ratio = win_ratio
-            pickle.dump(net_params, open(root_data_file + prefix +"best_policy_{}_epochs_{}.model".format(self.policy_value_net, epochs), 'wb'),
-                        pickle.HIGHEST_PROTOCOL)  # update the best_policy
-            if self.best_win_ratio == 1.0 and self.pure_mcts_playout_num < 5000:
-                self.pure_mcts_playout_num += 1000  # 增强
-                self.best_win_ratio = 0.0
+            self.config.best_win_ratio = win_ratio
+            pickle.dump(self.config, open("config-epochs-{0}-{1:.2f}.pkl".format(epochs,win_ratio), 'wb'))
+
+            if self.config.best_win_ratio == 1.0 and self.config.pure_mcts_playout_num < 5000:
+                self.config.pure_mcts_playout_num += 1000  # 增强
+                self.config.best_win_ratio = 0.0
 
     def run(self):
         """run the training pipeline"""
-        loss_records = []
+        print ("start training from game:{}".format(self.config.start_game_num))
         try:
-            for i in range(self.game_batch_num):
-                self.self_play(self.play_batch_size) # big step 1
-                print("iteration i:{}, episode_len:{}, augmented_len:{}, current_buffer_len:{}".format(i + 1, self.episode_len, self.augmented_len, len(self.data_buffer)))
+            for i in range(self.config.start_game_num, self.config.game_batch_num):
 
-                if len(self.data_buffer) > self.batch_size:
+                self.self_play(self.config.play_batch_size) # big step 1
+                print("iteration i:{}, episode_len:{}, augmented_len:{}, current_buffer_len:{}".format(i + 1,
+                                                            self.episode_len, self.augmented_len, len(self.config.data_buffer)))
+
+                if len(self.config.data_buffer) > self.config.batch_size:
                     loss_info = self.optimize(iteration=i) # big step 2
-                    loss_records.append(loss_info)
+                    self.config.loss_records.append(loss_info)
+
+                self.config.start_game_num = i + 1  # update for restart
 
                 # check the performance of the current model，and save the model params
-                if (i + 1) % self.check_freq == 0:
-                    print("current self-play batch: {}".format(i + 1))
+                if (i + 1) % self.config.check_freq == 0:
+                    print("current iteration: {}".format(i + 1))
                     win_ratio = self.evaluate() #big step 3
                     self.save_model(win_ratio, i+1)
 
-                # 每500轮保存下损失和熵
-                if  (i + 1) % 500 == 0:
-                    pickle.dump(loss_records, open(root_data_file+"loss_info_records.data", 'wb'),
-                                pickle.HIGHEST_PROTOCOL)
+
+
         except KeyboardInterrupt:
             print('\n\rquit')
 
 
 if __name__ == '__main__':
-    training_pipeline = TrainPipeline(init_model=None)
+    #config = pickle.load(open('config-epochs-{0}-{1:.2f}.pkl'.format(4, 0.800),'rb'))
+    config = None
+    training_pipeline = TrainPipeline(config=None)
     training_pipeline.run()
