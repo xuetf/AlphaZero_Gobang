8e6de4fafdda28bd9b6b610205b49eb8dce26fb7
xuetf <476122294@qq.com>
2018-05-11 21:57:39 +0800

    check loss

--- Config.py
+++ Config.py
@@ -10,9 +10,9 @@ tmp_data_file = "tmp/"
 
 class Config:
     def __init__(self):
-        self.board_width = 6
-        self.board_height = 6
-        self.n_in_row = 4
+        self.board_width = 8
+        self.board_height = 8
+        self.n_in_row = 5
         self.board = Board(width=self.board_width, height=self.board_height, n_in_row=self.n_in_row)
         self.game = Game(self.board)
         # training params
