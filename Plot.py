f159c50ee7f2ed6d82961605e2e73a6d5441644b
xuetf <476122294@qq.com>
2018-05-16 22:56:13 +0800

    pic

--- Plot.py
+++ Plot.py
@@ -2,6 +2,30 @@ import matplotlib.pyplot as plt
 from Config import *
 from Util import load_config
 
+def draw_pk():
+    plt.figure(figsize=(10, 10))
+    plt.suptitle('PK')
+    plt.xlabel('opponent')
+    plt.ylabel('times')
+
+    ax = plt.subplot(2, 2, 1)
+    ax.set_title('AlphaGobangZero VS RandomRolloutMCTS')
+    names = ['Win', 'Lose', 'Tie']
+    x = range(len(names))
+    y = [50, 0., 0.]
+    plt.bar(range(len(names)), y)
+    plt.xticks(x, names, rotation=45)
+    plt.yticks(range(0, 51, 10))
+
+    ax = plt.subplot(2, 2, 2)
+    ax.set_title('AlphaGobangZero VS Human')
+    names = ['Win', 'Lose', 'Tie']
+    x = range(len(names))
+    y = [19, 18, 13]
+    plt.bar(range(len(names)), y)
+    plt.xticks(x, names, rotation=45)
+    plt.yticks(range(0, 51, 10))
+    plt.show()
 
 
 def draw_loss():
@@ -20,4 +44,110 @@ def draw_loss():
     plt.legend()
     plt.show()
 
-draw_loss()
+def draw_epsilon_parameters():
+    '''data according to experiment'''
+    plt.figure(figsize=(10, 10))
+    plt.suptitle(r'$\epsilon$ experiment')
+    plt.xlabel('opponent')
+    plt.ylabel('win ratio')
+
+    ax = plt.subplot(2, 3, 1)
+    ax.set_title(r'$\epsilon$=0 AlphaGobangZero')
+    names = [r'$\epsilon$=0.2', r'$\epsilon$=0.8', 'Random', 'Human']
+    x = range(len(names))
+    y = [0.167, 0.333, 0.70, 0.30]
+    plt.bar(range(len(names)), y)
+    plt.xticks(x, names, rotation=45)
+    plt.yticks(np.arange(0, 1.01, 0.1))
+
+    ax = plt.subplot(2, 3, 2)
+    ax.set_title(r'$\epsilon$=0.2 AlphaGobangZero')
+    names = [r'$\epsilon$=0', r'$\epsilon$=0.8', 'Random', 'Human']
+    x = range(len(names))
+    y = [0.833, 0.80, 1.00, 0.50]
+    plt.bar(range(len(names)), y)
+    plt.xticks(x, names, rotation=45)
+    plt.yticks(np.arange(0, 1.01, 0.1))
+
+    ax = plt.subplot(2, 3, 3)
+    ax.set_title(r'$\epsilon$=0.8 AlphaGobangZero')
+    names = [r'$\epsilon$=0', r'$\epsilon$=0.2', 'Random', 'Human']
+    x = range(len(names))
+    y = [0.667, 0.20, 0.90, 0.333]
+    plt.bar(range(len(names)), y)
+    plt.xticks(x, names, rotation=45)
+    plt.yticks(np.arange(0, 1.01, 0.1))
+    plt.show()
+
+
+def draw_n_parameters():
+    '''data according to experiment'''
+    plt.figure(figsize=(10, 10))
+    plt.suptitle('n experiment')
+    plt.xlabel('opponent')
+    plt.ylabel('win ratio')
+
+    ax = plt.subplot(2, 3, 1)
+    ax.set_title('n=10 AlphaGobangZero')
+    names = ['n=400', 'n=1000', 'Random', 'Human']
+    x = range(len(names))
+    y = [0., 0., 0.6, 0.]
+    plt.bar(range(len(names)), y)
+    plt.xticks(x, names, rotation=45)
+    plt.yticks(np.arange(0, 1.01, 0.1))
+
+    ax = plt.subplot(2, 3, 2)
+    ax.set_title('n=400 AlphaGobangZero')
+    names = ['n=10', 'n=1000', 'Random', 'Human']
+    x = range(len(names))
+    y = [1.0, 0.4, 1.00, 0.50]
+    plt.bar(range(len(names)), y)
+    plt.xticks(x, names, rotation=45)
+    plt.yticks(np.arange(0, 1.01, 0.1))
+
+    ax = plt.subplot(2, 3, 3)
+    ax.set_title('n=1000 AlphaGobangZero')
+    names = ['n=10', 'n=400', 'Random', 'Human']
+    x = range(len(names))
+    y = [1.0, 0.6, 1.00, 0.60]
+    plt.bar(range(len(names)), y)
+    plt.xticks(x, names, rotation=45)
+    plt.yticks(np.arange(0, 1.01, 0.1))
+    plt.show()
+
+def draw_cpuct_parameters():
+    '''data according to experiment'''
+    plt.figure(figsize=(10, 10))
+    plt.suptitle(r'$c_{puct}$ experiment')
+    plt.xlabel('opponent')
+    plt.ylabel('win ratio')
+
+    ax = plt.subplot(2, 3, 1)
+    ax.set_title(r'$c_{puct}$=1 AlphaGobangZero')
+    names = [r'$c_{puct}$=5', r'$c_{puct}$=20', 'Random', 'Human']
+    x = range(len(names))
+    y = [0.367, 0.40, 1.00, 0.20]
+    plt.bar(range(len(names)), y)
+    plt.xticks(x, names, rotation=45)
+    plt.yticks(np.arange(0, 1.01, 0.1))
+
+    ax = plt.subplot(2, 3, 2)
+    ax.set_title(r'$c_{puct}$=5 AlphaGobangZero')
+    names = [r'$c_{puct}$=1', r'$c_{puct}$=20', 'Random', 'Human']
+    x = range(len(names))
+    y = [0.633, 0.80, 1.00, 0.50]
+    plt.bar(range(len(names)), y)
+    plt.xticks(x, names, rotation=45)
+    plt.yticks(np.arange(0, 1.01, 0.1))
+
+    ax = plt.subplot(2, 3, 3)
+    ax.set_title(r'$c_{puct}$=20 AlphaGobangZero')
+    names = [r'$c_{puct}$=1', r'$c_{puct}$=5', 'Random', 'Human']
+    x = range(len(names))
+    y = [0.60, 0.20, 1.00, 0.333]
+    plt.bar(range(len(names)), y)
+    plt.xticks(x, names, rotation=45)
+    plt.yticks(np.arange(0, 1.01, 0.1))
+    plt.show()
+
+draw_pk()
