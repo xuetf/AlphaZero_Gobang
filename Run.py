a4e3791bacaa227f56af278d8cb757a68ab62be2
xuetf <476122294@qq.com>
2018-05-17 19:39:46 +0800

    tour

--- Run.py
+++ Run.py
@@ -24,7 +24,7 @@ def run(config=None):
                                      Network=config.network, net_params=config.policy_param) # setup which Network to use based on the net_params
 
         mcts_player = AlphaZeroPlayer(best_policy.predict, c_puct=config.c_puct,
-                                 nplays=1200)  #set larger nplays for better performance
+                                 nplays=100)  #set larger nplays for better performance
 
         # uncomment the following line to play with pure MCTS
         # mcts_player2 = RolloutPlayer(nplays=1000, c_puct=config.c_puct)
@@ -41,25 +41,26 @@ def run(config=None):
 def tour(n_games=10):
     name = '../drive/workspace/work_deep_learning/tmp_5_in_rows_resnet2/epochs-{}-opponent-Pure-win-1.00.pkl'
     win_ratio = collections.defaultdict(float)
-    player2 = load_player_from_file(name.format(1500), add_noise=True, nplays=500)  # 最终模型
+    player2 = load_player_from_file(name.format(1500), add_noise=True, nplays=100)  # 最终模型
     for i in range(50, 1501, 50):
         win_cnt = collections.defaultdict(int)
         print ('AlphaGoZero{}'.format(i) + ' VS ' 'AlphaGoZero1500')
-        player1 = load_player_from_file(name.format(i), add_noise=True, nplays=500)
+        player1 = load_player_from_file(name.format(i), add_noise=True, nplays=100)
         for num in range(n_games):
             board = Board(width=8, height=8, n_in_row=5)
             game = Game(board)
             winner = game.start_game(player1, player2, who_first=num % 2, is_shown=0)
             win_cnt[winner] += 1
+            print ('Game {}, Winner No is {}'.format(num+1, winner))
         print("win: {}, lose: {}, tie:{}".format(win_cnt[1], win_cnt[2], win_cnt[-1]))
         win_ratio[i] = 1.0 * (win_cnt[1] + 0.5 * win_cnt[-1]) / n_games
     pickle.dump(dict(win_ratio), open('win_ratio.pkl','wb'))
     return win_ratio
 
 if __name__ == '__main__':
-    # config = load_config(file_name=tmp_data_file + 'epochs-1450-resnet2.pkl', only_load_param=False)
-    # run(config)
-    tour()
+    config = load_config(file_name=tmp_data_file + 'epochs-1450-resnet2.pkl', only_load_param=False)
+    run(config)
+
 
 
 
